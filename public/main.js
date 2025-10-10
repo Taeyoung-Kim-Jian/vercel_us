69acd1a7d9b04905bc72cb8133aab7dbcc221f1b
Taeyoung-Kim-Jian <62293421+Taeyoung-Kim-Jian@users.noreply.github.com>
2025-10-10 16:21:40 +0900

    Update main.js

--- public/main.js
+++ public/main.js
@@ -62,9 +62,9 @@ document.addEventListener("DOMContentLoaded", async () => {
           <td class="clickable-name" data-code="${r.종목코드}" data-name="${r.종목명}">
             ${SWINGINV.esc(r.종목명)}
           </td>
-          <td style="text-align:right">${SWINGINV.nf(r.시작가격)}</td>
-          <td style="text-align:right">${SWINGINV.nf(r.현재가격)}</td>
-          <td style="text-align:right">${SWINGINV.fmtPct(r.수익률)}</td>
+          <td style="text-align:ceter">${SWINGINV.nf(r.시작가격)}</td>
+          <td style="text-align:ceter">${SWINGINV.nf(r.현재가격)}</td>
+          <td style="text-align:ceter">${SWINGINV.fmtPct(r.수익률)}</td>
         </tr>
       `
         )
@@ -101,9 +101,9 @@ document.addEventListener("DOMContentLoaded", async () => {
           <td class="clickable-name" data-code="${r.종목코드}" data-name="${r.종목명}">
             ${SWINGINV.esc(r.종목명)}
           </td>
-          <td style="text-align:right">${SWINGINV.nf(r.적정매수가)}</td>
-          <td style="text-align:right">${SWINGINV.nf(r.현재가)}</td>
-          <td style="text-align:right">${SWINGINV.fmtPct(r.괴리율)}</td>
+          <td style="text-align:ceter">${SWINGINV.nf(r.적정매수가)}</td>
+          <td style="text-align:ceter">${SWINGINV.nf(r.현재가)}</td>
+          <td style="text-align:ceter">${SWINGINV.fmtPct(r.괴리율)}</td>
         </tr>
       `
         )
